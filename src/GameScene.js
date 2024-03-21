33bf4fc65534fc66001ecba2677bc18e572b9a58
ss-netigo <soroush.shalali@netigo.de>
2024-03-21 11:37:41 +0100

    defined class GameScene;

---
+++ src/GameScene.js
@@ -0,0 +1,124 @@
+import Phaser from 'phaser';
+
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'GameScene' });
+        this.score = 0;
+        this.isGameRunning = false;
+    }
+
+    preload() {
+        this.load.image('bg', 'src/img/bg-game.png');
+        this.load.atlas('elements-atlas', 'src/spritesheets/game-elements.png', 'src/spritesheets/game-elements.json');
+    }
+
+    create() {
+        this.add.image(480, 540, 'bg').setOrigin(0.5, 1);
+        const obstacle1 = this.add.image(780, 540, 'elements-atlas', 'obstacle1').setOrigin(0.5, 1);
+        const obstacle2 = this.add.image(480, 540, 'elements-atlas', 'obstacle2').setOrigin(0.5, 1);
+        const obstacle3 = this.add.image(120, 540, 'elements-atlas', 'obstacle3').setOrigin(0.5, 1);
+    
+    
+        this.enemysOffset = 100;
+        this.obstacle1EnemyY = (obstacle1.y - obstacle1.height) + this.enemysOffset;
+    
+        this['enemy1'] = this.add.image(180, this.obstacle1EnemyY, 'elements-atlas', 'enem1').setOrigin(0.5, 0);
+        this['enemy2'] = this.add.image(80, this.obstacle1EnemyY, 'elements-atlas', 'enem2').setOrigin(0.5, 0);
+        this['enemy3'] = this.add.image(80, this.obstacle1EnemyY, 'elements-atlas', 'enem3').setOrigin(0.5, 0);
+    
+        obstacle1.setDepth(1);
+        obstacle2.setDepth(1);
+        obstacle3.setDepth(1);
+    
+        this.enemy1.setInteractive();
+        this.enemy2.setInteractive();
+        this.enemy3.setInteractive();
+    
+        
+        this.input.on('gameobjectdown', (pointer, gameObject, event)=> {
+            this.updateScore();
+            gameObject.disableInteractive();
+    
+            setTimeout(() => {
+                gameObject.setInteractive();
+            }, 1000);
+        });
+    
+        document.getElementById('start').addEventListener('click', ()=>{
+            this.isGameRunning = !this.isGameRunning;
+            console.log(this.isGameRunning);
+        })
+    
+        this.delay = 2000;
+        this.time.addEvent({
+            delay: this.delay,
+            callback: this.startRandomMovement,
+            callbackScope: this,
+            loop: true
+        });
+    }
+
+    setupInputEvents() {}
+
+    setupTimer() {
+        this.delay = 2000;
+        this.time.addEvent({
+            delay: this.delay,
+            callback: this.startRandomMovement,
+            callbackScope: this,
+            loop: true
+        });
+    }
+
+    startRandomMovement() {
+
+        if (!this.isGameRunning) {
+            return;
+        }
+    
+        const obstacleNumber =  Phaser.Math.Between(0, 2);
+    
+        let obstaclesX = [780, 480, 120];
+    
+        const positionX =  obstaclesX[obstacleNumber];
+        const randomNum = Phaser.Math.Between(1, 3);
+    
+        this.tweens.add({
+            targets: this['enemy' + randomNum],
+            x: positionX,
+            duration: 1,
+            ease: 'Linear',
+            onComplete: () => {
+                this.tweens.add({
+                    targets: this['enemy' + randomNum],
+                    y: this['enemy' + randomNum].y - 200,
+                    duration: this.delay / 3,
+                    ease: 'Linear',
+                    onComplete: () => {
+                        this.tweens.add({
+                            targets: this['enemy' + randomNum],
+                            y: this['enemy' + randomNum].y + 201,
+                            duration: this.delay / 3,
+                            ease: 'Linear',
+                            delay: this.delay / 3,
+                            onComplete: () => {
+                                if (this.clickedObject != undefined) {
+                                    if (this.clickedObject.input) {
+                                        this.clickedObject.setInteractive();
+                                    }
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    updateScore() {
+        this.score++;
+        console.log('score:', this.score);
+    }
+
+    update() {}
+}
