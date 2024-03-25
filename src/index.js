e2192997f40e6c2bdcc7fef58d789dec320c9222
ss-netigo <soroush.shalali@netigo.de>
2024-03-25 17:16:39 +0100

    renderd data in highscore_view;

--- src/index.js
+++ src/index.js
@@ -43,16 +43,35 @@ let highScores;
 document.getElementById('instructions_btn').addEventListener('click', async () => {
     try {
         const response = await fetch('http://localhost:3001/data');
-
+        
         if (!response.ok) {
             throw new Error('Network response was not ok!');
         }
-
+        
         highScores = await response.json();
-        console.log('Server response:', highScores);
     } catch (error) {
         console.error('Error:', error);
     }
+
+    let rowsElements = `
+    <tr>
+        <th>Id</th>
+        <th>Nickname</th>
+        <th>Score</th>
+    </tr>
+    `;
+    highScores.forEach((row)=>{
+        rowsElements += `
+        <tr>
+            <td>${row.id}</td>
+            <td>${row.name}</td>
+            <td>${row.id}</td>
+        </tr>
+        `;
+    });
+
+    let table = document.getElementById('highscores_table');
+    table.innerHTML += rowsElements;
 });
 
-console.log(highScores);
+
